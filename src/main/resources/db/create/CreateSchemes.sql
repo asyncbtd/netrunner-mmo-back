5f9660ecf595ac1c60ff890dc77bd075c29cff07
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-09-12 19:20:33 +0300

    creating docker-compose for local development

---
+++ src/main/resources/db/create/CreateSchemes.sql
@@ -0,0 +1,4 @@
+create schema attribute_schema authorization nrmmo;
+create schema audit_schema authorization nrmmo;
+create schema nrmmo_schema authorization nrmmo;
+create schema reference_schema authorization nrmmo;
